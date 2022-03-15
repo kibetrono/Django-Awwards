910e422779d8d3fda1a0ca5a9777c388d5bb4c24
email <kibetdavidro@gmail.com>
2022-03-15 20:54:00 +0300

    add project  model class test

--- app/tests.py
+++ app/tests.py
@@ -32,3 +32,33 @@ class ProfileTestClass(TestCase):
     def tearDown(self):
         """tearDown method"""
         Profile.objects.all().delete()
+
+
+class ProjectTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        """Creating a new location and saving it"""
+        user = User.objects.create(username='kibet',first_name='rono',last_name='David')
+
+        self.project=Project.objects.create(user=user,title="project 1",description="Initial project",image='lorem2.png',url="https://ksms.or.ke/contact-us-3/",location="New description",date=None)
+        self.project.save_project()
+
+    def test_instance(self):
+        """Testing instance"""
+        self.assertTrue(isinstance(self.project, Project))
+
+    def test_save_method(self):
+        """Testing Save Method"""
+        self.project.save_project()
+        project = Project.objects.all()
+        self.assertTrue(len(project) > 0)
+
+    def test_delete_method(self):
+        """Testing delete Method"""
+        self.project.delete_project()
+        project= Project.objects.all()
+        self.assertTrue(len(project) < 1)
+
+    def tearDown(self):
+        """tearDown method"""
+        Project.objects.all().delete()
