b21f8d0a24051278d75580250a93fe8f96708357
email <kibetdavidro@gmail.com>
2022-03-16 14:47:41 +0300

    deployment to heroku

--- app/views.py
+++ app/views.py
@@ -18,7 +18,7 @@ from django.http import HttpResponse
 def home(request):  # Home page
     project = Project.objects.all()
     recent_project = Project.objects.all()
-    rating = Rating.objects.filter(project_id=recent_project.id).first()
+    rating = Rating.objects.filter(project_id=recent_project).first()
     context={"projects": project, "project_home": recent_project, "rating": rating}
     return render(request, "app/index.html",context)
 
